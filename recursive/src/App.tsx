2d011350437570703b9e4d1194226eb842ac1363
Haris Amjad <harisamjad56@gmail.com>
2024-11-25 16:39:18 +0500

    Responsive

--- recursive/src/App.tsx
+++ recursive/src/App.tsx
@@ -15,9 +15,9 @@ const App = () => {
   const departments: Department[] = data;
 
   return (
-    <div className="select-none px-12 py-12 flex flex-col gap-6 cursor-default">
+    <div className="select-none px-24 py-12 max-lg:px-12 max-md:px-6 max-sm:px-4 flex flex-col gap-6 cursor-default">
       <h1 className="text-2xl font-semibold">Recursive React Component</h1>
-      <div className="flex flex-col gap-1 w-96 border rounded-lg px-4 py-2">
+      <div className="flex flex-col gap-1 max-sm:gap-2 w-96 max-sm:w-full border rounded-lg px-4 py-2">
         {departments.map((department) => (
           department.name && <Department department={department} key={department.name} />
         ))}
@@ -41,11 +41,11 @@ const Department = ({ department }: { department: Department }) => {
         ) : (
           <IoMdPerson className="text-neutral-800 size-5" />
         )}
-        <p key={department.name}>{department.name}</p>
+        <p className="text-base max-sm:text-xl" key={department.name}>{department.name}</p>
       </div>
 
       {open && department.departments?.map((subdepartment: Department) => (
-        <div className="ml-6 flex flex-col gap-1" key={subdepartment.name}>
+        <div className="ml-6 flex flex-col gap-1 max-sm:gap-2" key={subdepartment.name}>
           {subdepartment.name && <Department department={subdepartment} key={subdepartment.name} />}
         </div>
       ))}
